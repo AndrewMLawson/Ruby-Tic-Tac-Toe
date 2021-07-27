7c294a0ecd6508ac77547832ea3b75c0350e6d64
Andrew Lawson <andrewlawson234@gmail.com>
2021-07-27 06:13:36 -0700

    Set up printable and dynamically changing tic-tac-toe board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,47 +1,45 @@
-#      1    2   3
-# c      |    |
-#     -------------
-# b      |    |
-#     -------------
-# a      |    |   
-
 class GameBoard
-    attr_accessor  :row1
-    attr_accessor  :row2
-    attr_accessor  :row3
+    attr_accessor  :rows
     def initialize()
-        @row1 = Array.new(3)
-        @row2 = Array.new(3)
-        @row3 = Array.new(3)
-    end
-
-    def board_status()
-        p @row1
-        p @row2
-        p @row3
-    end
-    
-    def print_gameboard()
-        p ""
+        @rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     end
 end
 
 class Player
-    def initialize(marker)
-     @marker = marker
+    def initialize(name, marker)
+        @name = name
+        @marker = marker
+    end
+end
+
+class Game 
+    def initialize()
+        @board = GameBoard.new()
+        puts "Player 1, what is your name?"
+        player1_name = gets.chomp
+        p "#{player1_name}, what letter or special character do you want to use for your marker?"
+        player1_marker = gets.chomp
+        p "Player 2, what is your name?"
+        player2_name = gets.chomp
+        p "#{player2_name}, what letter or special character do you want to use for your marker?"
+        player2_marker = gets.chomp
+        @player1 = Player.new(player1_name, player1_marker)
+        @player2 = Player.new(player2_name, player2_marker)
+        print_current_board()
     end
 
-    def access?()
-        p game1.row2
+    def print_current_board()
+        puts "        #  #{@board.rows[0][0]} | #{@board.rows[0][1]} | #{@board.rows[0][2]}
+        # ---+---+---
+        #  #{@board.rows[1][0]} | #{@board.rows[1][1]} | #{@board.rows[1][2]}
+        # ---+---+---
+        #  #{@board.rows[2][0]} | #{@board.rows[2][1]} | #{@board.rows[2][2]}"
+
     end
 
-    def place_marker(x, y)
-        #
-    end
- end 
-
-game1 = GameBoard.new()
-player1 = Player.new('X')
-player1.access?
+    
+    
+end
 
 
+game = Game.new()
