d5302cb490f7efcae0630b8de0df6369e7c1b64e
Andrew Lawson <andrewlawson234@gmail.com>
2021-07-27 07:46:04 -0700

    Add marker to board functionality

--- tictactoe.rb
+++ tictactoe.rb
@@ -6,6 +6,7 @@ class GameBoard
 end
 
 class Player
+    attr_reader :name, :marker
     def initialize(name, marker)
         @name = name
         @marker = marker
@@ -14,7 +15,6 @@ end
 
 class Game 
     def initialize()
-        @board = GameBoard.new()
         puts "Player 1, what is your name?"
         player1_name = gets.chomp
         p "#{player1_name}, what letter or special character do you want to use for your marker?"
@@ -25,7 +25,9 @@ class Game
         player2_marker = gets.chomp
         @player1 = Player.new(player1_name, player1_marker)
         @player2 = Player.new(player2_name, player2_marker)
-        print_current_board()
+        @board = GameBoard.new()
+        @player1_turn = true
+        play_turn()
     end
 
     def print_current_board()
@@ -37,8 +39,38 @@ class Game
 
     end
 
-    
-    
+    def play_turn()
+        print_current_board()
+        if @player1_turn
+            @player1_turn = false
+            p "#{@player1.name}, choose your space!"
+            move = gets.chomp.to_i
+            add_marker(move, @player1.marker)
+        else
+            @player1_turn = true
+            p "#{@player2.name}, choose your space!"
+            move = gets.chomp.to_i
+            add_marker(move, @player2.marker)
+        end
+    end
+
+    def add_marker(move, marker)
+        i = 0
+        while i < 3 do
+            if @board.rows[i].index(move)
+                to_replace = @board.rows[i].index(move)
+                @board.rows[i][to_replace] = marker
+                play_turn()
+                break
+            else
+                i += 1
+            end
+        end
+    end
+
+    # def win_check()
+
+    # end
 end
 
 
